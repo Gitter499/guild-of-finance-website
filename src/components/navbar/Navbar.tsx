0e06a743f3ed33e811e6b89e7457b360b7d227fd
Gitter499 <rafayel.amirkhanyan@gmail.com>
2021-03-07 13:44:16 -0500

    added navbar component

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react'
+
+export default function Navbar() {
+    return (
+        <div>
+           <h2>
+               Hello World
+           </h2>
+        </div>
+    )
+}
